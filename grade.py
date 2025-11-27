3f4514e18ec59a83b9626bdf8d8b8d77f2fcfc82
awalikar886 <awalikar886@gmail.com>
2025-11-27 14:46:26 +0530

    Update grade.py

--- grade.py
+++ grade.py
@@ -1,32 +1,49 @@
-import sys
+ import sys
 
-if len(sys.argv) != 6:
-    print("grade evaluation")
+# Check if user provided 5 marks through command line
+if len(sys.argv) == 6:
+    print("User provided marks through command line:")
 
-name =(sys.argv[1])
-sub1 = float(sys.argv[2])
-sub2 = float(sys.argv[3])
-sub3 = float(sys.argv[4])
-sub4 = float(sys.argv[5])
- 
-total=sub1 + sub2 + sub3 + sub4;
-average= total / 4
+    script_name = sys.argv[0]
+    sub1 = float(sys.argv[1])
+    sub2 = float(sys.argv[2])
+    sub3 = float(sys.argv[3])
+    sub4 = float(sys.argv[4])
+    sub5 = float(sys.argv[5])
 
-if(average<=100):
-   grade= "a"
-elif(average<=80):
-    grade ="b"
-elif(average<=70):
-    grade= "c"
-elif(average<=60):
-    grade ="d"
 else:
-    grade ="fail"
+    print("No input given - using default values:")
 
-print("name",name)
-print("sub1",sub1)
-print("sub2",sub2)
-print("sub3",sub3)
-print("sub4",sub4)
-print("average",average)
-print("grade",grade)
+    script_name = sys.argv[0]
+    sub1 = 40
+    sub2 = 70
+    sub3 = 40
+    sub4 = 60
+    sub5 = 10
+
+# Calculate total & average
+total = sub1 + sub2 + sub3 + sub4 + sub5
+average = total / 5
+
+# Determine grade
+if average >= 90:
+    grade = "A"
+elif average >= 75:
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
+
+# Print results
+print("\n--- Student Result ---")
+print("Subject 1:", sub1)
+print("Subject 2:", sub2)
+print("Subject 3:", sub3)
+print("Subject 4:", sub4)
+print("Subject 5:", sub5)
+print("Total Marks:", total)
+print("Average Marks:", average)
+print("Grade:", grade)
